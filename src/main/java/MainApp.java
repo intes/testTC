d2fd67b16ce66daa57ef934c663af35ae15bc3c1
sa <sa@intes.pro>
2017-09-30 18:26:11 +0300

    Initial commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,9 @@
+/**
+ * Created by andreevs on 30.09.17.
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        pro.intes.tests.TestTeamCity tc = new pro.intes.tests.TestTeamCity();
+        tc.printHello();
+    }
+}
---
+++ src/main/java/pro/intes/tests/TestTeamCity.java
@@ -0,0 +1,10 @@
+package pro.intes.tests;
+
+/**
+ * Created by andreevs on 30.09.17.
+ */
+public class TestTeamCity {
+    public void printHello() {
+        System.out.println("Hi there!!!");
+    }
+}
